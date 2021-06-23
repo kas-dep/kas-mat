65477926b085db041cafbad3d02030c798f88c08
Mattdev92 <mateuszgrzejszczyk1@gmail.com>
2021-06-23 15:15:17 +0200

    add nav and some changes

--- index.htm/index.js
+++ index.htm/index.js
@@ -1,4 +1,11 @@
-const element = document.querySelector('.element');
-element.addEventListener('click',()=>{
-    alert('Element 3 clicked');
-})
+const element = document.querySelector(".element");
+element.addEventListener("click", () => {
+  alert("Element 3 clicked");
+});
+const title = document.querySelector(".title");
+title.addEventListener("mouseover", () => {
+  title.classList.add("over");
+});
+title.addEventListener("mouseleave", () => {
+  title.classList.remove("over");
+});
